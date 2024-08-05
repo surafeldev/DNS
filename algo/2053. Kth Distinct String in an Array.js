82de282e74f09f88f1f8f2d60fa303f1e706a519
surafeldev <surafel.nigusie.dev@gmail.com>
2024-08-05 23:44:34 +0300

    update change

--- algo/2053. Kth Distinct String in an Array.js
+++ algo/2053. Kth Distinct String in an Array.js
@@ -1,8 +1,6 @@
-/**
- * @param {string[]} arr
- * @param {number} k
- * @return {string}
- */
+const { expect } = require('chai');
+
+// The function to be tested
 var kthDistinct = function(arr, k) {
     let map = new Map();
     for (let i = 0; i < arr.length; i++) {
@@ -12,7 +10,7 @@ var kthDistinct = function(arr, k) {
             map.set(arr[i], 1);
         }
     }
-    let keys = Array.from(map.keys()).sort();
+    let keys = Array.from(map.keys());
     for (let i = 0; i < keys.length; i++) {
         if (map.get(keys[i]) === 1) {
             k--;
@@ -22,5 +20,42 @@ var kthDistinct = function(arr, k) {
         }
     }
     return '';
-    
 };
+
+// Unit tests
+describe('kthDistinct', function() {
+    it('should return the 2nd distinct string "a" from ["d","b","c","b","c","a"]', function() {
+        let arr = ["d","b","c","b","c","a"];
+        let k = 2;
+        let result = kthDistinct(arr, k);
+        expect(result).to.equal("a");
+    });
+
+    it('should return the 1st distinct string "aaa" from ["aaa","aa","a"]', function() {
+        let arr = ["aaa","aa","a"];
+        let k = 1;
+        let result = kthDistinct(arr, k);
+        expect(result).to.equal("aaa");
+    });
+
+    it('should return an empty string when there are fewer than 3 distinct strings in ["a","b","a"]', function() {
+        let arr = ["a","b","a"];
+        let k = 3;
+        let result = kthDistinct(arr, k);
+        expect(result).to.equal("");
+    });
+
+    it('should return the 1st distinct string "a" from ["a","b","c"]', function() {
+        let arr = ["a","b","c"];
+        let k = 1;
+        let result = kthDistinct(arr, k);
+        expect(result).to.equal("a");
+    });
+
+    it('should return an empty string for an empty array', function() {
+        let arr = [];
+        let k = 1;
+        let result = kthDistinct(arr, k);
+        expect(result).to.equal("");
+    });
+});
